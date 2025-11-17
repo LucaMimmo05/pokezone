27a39047b3d79c5197bb8e08b80fca9491cb50e2
Sofia Palmeri <sofia.palmeri@itsincom.it>
2025-11-17 18:00:29 +0100

    Implemented display of some pokemon details retrieved from the API

--- src/app/pokemon-details/pokemon-details.ts
+++ src/app/pokemon-details/pokemon-details.ts
@@ -1,13 +1,25 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { PokezoneService } from '../services/pokezone-service';
+import { Pokemon } from '../models/pokemon-details/pokemon';
 
 @Component({
   selector: 'app-pokemon-details',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './pokemon-details.html',
   styleUrl: './pokemon-details.css'
 })
 export class PokemonDetails {
-    private readonly route = inject(ActivatedRoute);        // Per accedere ai parametri della rotta
+    private readonly route = inject(ActivatedRoute);
+    private readonly pokezoneService = inject(PokezoneService);
+    pokemon: Pokemon | null = null;
+    id: number = 4;
 
+    async ngOnInit() {
+      this.loadPokemonDetails();
+    }
+
+    async loadPokemonDetails() {
+      this.pokemon = await this.pokezoneService.getPokemonDetails(this.id);
+    }
 }
--- src/app/services/pokezone-service.ts
+++ src/app/services/pokezone-service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
-import { PokemonDetails } from '../pokemon-details/pokemon-details';
+import { Pokemon } from '../models/pokemon-details/pokemon';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +9,9 @@ import { PokemonDetails } from '../pokemon-details/pokemon-details';
 export class PokezoneService {
   private readonly http = inject(HttpClient);
   private readonly baseUrl = 'https://pokeapi.co/api/v2';
-  
-  id = 1; // proprietà corretta
 
-  async getPokemonDetails(id: number): Promise<PokemonDetails> {
-    const response = this.http.get<PokemonDetails>(`${this.baseUrl}/pokemon/${id}`);
+  async getPokemonDetails(id: number): Promise<Pokemon> {
+    const response = this.http.get<Pokemon>(`${this.baseUrl}/pokemon/${id}`);
     return firstValueFrom(response);
-}
+  }
 }
