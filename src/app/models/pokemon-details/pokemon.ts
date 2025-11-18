688e4b9dae089d0ec29051b04796303941c56f6e
Sofia Palmeri <sofia.palmeri@itsincom.it>
2025-11-18 09:57:47 +0100

    Implemented additional interfaces in Pokemon

--- src/app/models/pokemon-details/pokemon.ts
+++ src/app/models/pokemon-details/pokemon.ts
@@ -1,7 +1,11 @@
 import { AbilityEntry } from "./ability-entry";
+import { GameIndex } from "./game-index";
 import { HeldItem } from "./held-item";
+import { MoveEntry } from "./move-entry";
 import { NamedAPIResource } from "./named-api-resource";
 import { PastType } from "./past-type";
+import { SpriteSet } from "./sprite-set";
+import { StatEntry } from "./stat-entry";
 import { TypeEntry } from "./type-entry";
 
 export interface Pokemon {
@@ -14,21 +18,29 @@ export interface Pokemon {
   is_default: boolean;
 
   abilities: AbilityEntry[];
+  
+  forms: NamedAPIResource[];
+  
+  species: NamedAPIResource; 
+  
+  sprites: SpriteSet;
 
   cries: {
     latest: string;
     legacy: string;
   };
 
-  forms: NamedAPIResource[];
-
-  types: TypeEntry[];
-
+  game_indices: GameIndex[];
+  
   held_items: HeldItem[];
-
+  
   location_area_encounters: string;
-
-  species: NamedAPIResource;
+  
+  moves: MoveEntry[];
   
   past_types: PastType[];
+  
+  stats: StatEntry[];
+
+  types: TypeEntry[];
 }
