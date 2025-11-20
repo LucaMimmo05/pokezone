6b3ffcff42f56bb3fcb0233d6c00c9c61634b552
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-11-20 11:59:51 +0100

    Refactor PokemonDetails component and restructure related files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Home } from './pages/home/home';
-import { PokemonDetails } from './pokemon-details/pokemon-details';
+import { PokemonDetails } from './pages/pokemon-details/pokemon-details';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: '', component: Home },
   {
-        path: 'pokemon',
-        component: PokemonDetails,
-        title: 'Dettagli Pokémon'
-    }
+    path: 'pokemon',
+    component: PokemonDetails },
 ];
