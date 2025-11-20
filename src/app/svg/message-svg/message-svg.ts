b96354d65534892a6227675e475db2886fae08aa
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-11-20 09:27:14 +0100

    Add dynamic color input to all SVG components

--- src/app/svg/message-svg/message-svg.ts
+++ src/app/svg/message-svg/message-svg.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-message-svg',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './message-svg.css',
 })
 export class MessageSVG {
-
+  color = input<string>('#E87878');
 }
