0a8bf9219ce5189fe147cae92d432d35c99d8d3b
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-11-20 11:54:42 +0100

    Update home route to use empty path for better navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { PokemonDetails } from './pokemon-details/pokemon-details';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'home', component: Home },
+  { path: '', component: Home },
   {
         path: 'pokemon',
         component: PokemonDetails,
