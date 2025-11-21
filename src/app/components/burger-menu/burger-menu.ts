4db038a3bab1957dddc04e607f7346b346af2a76
Giulia <giulia.zecchetin@gmail.com>
2025-11-21 10:30:41 +0100

    Created the burger menu for responsive.

--- src/app/components/burger-menu/burger-menu.ts
+++ src/app/components/burger-menu/burger-menu.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MENU_ITEMS } from '../../constraints/menu-items';
+import { MenuItem } from "../menu-item/menu-item";
 
 @Component({
   selector: 'app-burger-menu',
-  imports: [],
+  imports: [MenuItem],
   templateUrl: './burger-menu.html',
   styleUrl: './burger-menu.css',
 })
 export class BurgerMenu {
-
+    items = MENU_ITEMS;
 }
