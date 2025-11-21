dbba9d6b4bdd632ee73069e97c6921a2bfbb55fa
Sofia <palmeri.sofia@gmail.com>
2025-11-21 12:31:16 +0100

    Created the evolution chain component

---
+++ src/app/components/evolution-chain/evolution-chain.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evolution-chain',
+  imports: [],
+  templateUrl: './evolution-chain.html',
+  styleUrl: './evolution-chain.css',
+})
+export class EvolutionChain {
+
+}
