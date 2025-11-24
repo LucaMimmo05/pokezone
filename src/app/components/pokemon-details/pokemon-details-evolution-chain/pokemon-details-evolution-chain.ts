dd9b3be0529ec11395d5d91be240ad6f0c4d4255
gates-cmd <gianluca.ornago@itsincom.it>
2025-11-24 10:47:29 +0100

    refactored loadEvolutionChain method

--- src/app/components/pokemon-details/pokemon-details-evolution-chain/pokemon-details-evolution-chain.ts
+++ src/app/components/pokemon-details/pokemon-details-evolution-chain/pokemon-details-evolution-chain.ts
@@ -4,6 +4,7 @@ import { EvolutionStage } from '../../../models/pokemon-details/evolution-stage'
 import { EvolutionChain } from '../../../models/pokemon-details/evolution-chain';
 import { getDataFromUrl } from '../../../utils/url-utils';
 import { ArrowSvg } from '../../../svg/arrow-svg/arrow-svg';
+import { PokemonService } from '../../../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-details-evolution-chain',
@@ -21,36 +22,11 @@ export class PokemonDetailsEvolutionChain {
 
   evolutionPaths: EvolutionStage[][] = [];
 
+  constructor(private pokemonService: PokemonService) {}
+
   async ngOnInit() {
     if (!this.species?.url) return;
-    await this.loadEvolutionChain();
-  }
-
-  //TODO: spostare in service
-  async loadEvolutionChain() {
-    const speciesResponse = await fetch(this.species.url);
-    const speciesData = await speciesResponse.json();
-
-    const evolutionResponse = await fetch(speciesData.evolution_chain.url);
-    const evolutionChain: EvolutionChain = await evolutionResponse.json();
-
-    this.evolutionPaths = [];
-    this.createPaths(evolutionChain.chain, []);
-  }
-
-  //TODO: spostare in service
-  createPaths(stage: EvolutionStage, currentPath: EvolutionStage[]) {
-    const newPath = [...currentPath];
-    newPath.push(stage);
-
-    if (stage.evolves_to.length === 0) {
-      this.evolutionPaths.push(newPath);
-      return;
-    }
-
-    for (let nextStage of stage.evolves_to) {
-      this.createPaths(nextStage, newPath);
-    }
+    this.evolutionPaths = await this.pokemonService.getEvolutionChain(this.species.url);
   }
 
   getDataFromUtils(url: string): string {
