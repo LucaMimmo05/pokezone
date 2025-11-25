b6ad0c6d157e35200eadafcf74b6c7549f278803
LucaFerro <luca.ferro2003@gmail.com>
2025-11-25 10:54:14 +0100

    feat(loader): extract loader into reusable component and integrate on dashboard

--- src/app/components/loader/loader.ts
+++ src/app/components/loader/loader.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { PokeballSvg } from '../../svg/pokeball-svg/pokeball-svg';
 
 @Component({
   selector: 'app-loader',
-  imports: [],
+  imports: [PokeballSvg],
   templateUrl: './loader.html',
-  styleUrl: './loader.css',
+  styleUrl: './loader.css'
 })
-export class Loader {
-
-}
+export class LoaderComponent {}
