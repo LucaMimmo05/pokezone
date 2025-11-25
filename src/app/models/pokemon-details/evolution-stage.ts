7f611a2ffed914c4fda6aa36c63d4ae5ed566819
gates-cmd <gianluca.ornago@itsincom.it>
2025-11-25 09:40:44 +0100

    Evolutions can now handle user click and redirect to the clicked pokemon page.

--- src/app/models/pokemon-details/evolution-stage.ts
+++ src/app/models/pokemon-details/evolution-stage.ts
@@ -3,5 +3,4 @@ import { EvolutionSpecies } from "./evolution-species";
 export interface EvolutionStage {
     species: EvolutionSpecies;
     evolves_to: EvolutionStage[];
-    evolutionLevels: EvolutionStage[][];
 }
--- src/app/pages/pokemon-details/pokemon-details.ts
+++ src/app/pages/pokemon-details/pokemon-details.ts
@@ -33,14 +33,20 @@ export class PokemonDetails {
   private readonly router = inject(Router);
   private readonly pokezoneService = inject(PokemonService);
   pokemon: Pokemon | null = null;
-  id: number = Number(this.route.snapshot.paramMap.get('id'));
+  id!: number;
   category: any = null;
   weaknesses: string[] = [];
 
   async ngOnInit() {
-    await this.loadPokemonDetails();
-    await this.loadPokemonCategory();
-    await this.loadPokemonWeaknesses();
+    this.route.paramMap.subscribe(async params => {
+      this.id = Number(params.get('id'));
+
+      if (!this.id) return;
+
+      await this.loadPokemonDetails();
+      await this.loadPokemonCategory();
+      await this.loadPokemonWeaknesses();
+    });
   }
 
   async loadPokemonDetails() {
